2631b14fda889435a829fda8dad9c2211aa58a33
Shayaan Abdullah <sabdullah@berkeley.edu>
2015-10-10 19:29:59 -0700

    Correct error in algorithm

--- algs/cab/SourceZ.py
+++ algs/cab/SourceZ.py
@@ -6,7 +6,7 @@ class sourceZ(qdf.QDF2Distillate):
 	def initialize(self, section, name):
 		self.set_section(section)
 		self.set_name(name)
-		self.set_version(3)
+		self.set_version(4)
 		self.register_input("voltage_phase")
 		self.register_input("current_phase")
 		self.register_input("voltage_mag")
@@ -41,7 +41,7 @@ class sourceZ(qdf.QDF2Distillate):
 				continue
 			#now peform calculation and output stream
 			time = voltage_phase[i_vol_phase][0]
-			tanAng_reading = np.degrees(np.tan(voltage_phase[i_vol_phase][1] - current_phase[i_cur_phase][1]))
+			tanAng_reading = np.tan(voltage_phase[i_vol_phase][1] - current_phase[i_cur_phase][1])
 			MagZ_reading = np.divide(voltage_mag[i_vol_mag][1],current_mag[i_cur_mag][1])
 			tanAng.addreading(time,tanAng_reading)
 			MagZ.addreading(time,MagZ_reading)
