360c869ebda1b01485608aba8322737bc15fdfea
Omid <omid-cs@users.noreply.github.com>
2016-01-25 13:08:38 -0800

    update

--- algs/cab/phase_reverse.py
+++ algs/cab/phase_reverse.py
@@ -1,23 +1,20 @@
 import qdf
 import numpy as np
 
-class Phase_Reverse(qdf.QDF2Distillate):
-	
-	def initialize(self, section="Phase_Reverse", name="default"):
+class Phase_Reverse (qdf.QDF2Distillate):
+  def initialize(self, section="Phase_Reverse", name="default"):
 		self.set_section(section)
 		self.set_name(name)
 		self.set_version(1)
 		self.register_output("phase_reverse", "Degrees")
 		self.register_input("phase1")
 
-	def compute(self, changed_ranges, input_streams, params, report):
-		
+  def compute(self, changed_ranges, input_streams, params, report):
 		phase_reverse_output = report.output("phase_reverse")
 		phase1 = input_streams["phase1"]
 
 		i1 = 0
 		while i1 < len(phase1):
-
 			time = phase1[i1][0]
 			pr = (phase1[i1][1] + 180) % 360
 			phase_reverse_output.addreading(time,pr)
